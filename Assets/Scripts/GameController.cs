b5628ce58da54138c3f79475f92d7065119e560d
Ryan <obviouslyghosts@gmail.com>
2020-08-02 16:02:43 -0400

    simple title screen to Arena

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+  public static GameController instance;
+  public string arena;
+
+  private void Awake()
+  {
+
+    if (instance != null)
+    {
+      Destroy( gameObject );
+      return;
+    }
+    instance = this;
+    DontDestroyOnLoad( gameObject );
+  }
+
+  public void StartArena()
+  {
+    SceneManager.LoadScene( arena );    
+  }
+
+
+}
